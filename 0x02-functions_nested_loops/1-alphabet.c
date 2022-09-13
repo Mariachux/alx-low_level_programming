5e798f86590350a5e2d4314f4d82b39b38073fb4
Mariachux <ezehchukwuma@gmail.com>
2022-09-13 10:55:45 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,16 @@
 /**
  * main - check the code
  *  Return: Always 0.
- *  print_alphabet- prints the alphabets in lower case
+ *  print_alphabet - prints the alphabets in lower case
  */
 
 void print_alphabet(void)
 {
 
 	char ch;
+	
 	ch = 'a';
+	
 	while (ch <= 'z')
 	{
 		_putchar(ch);
