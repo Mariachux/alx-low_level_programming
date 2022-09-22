c84c3191b17893f7c287b447a189b8565098ab6d
Mariachux <ezehchukwuma@gmail.com>
2022-09-22 03:20:59 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,7 @@
  * @n: the number of bytes from src to be appended to dest
  * Return: a pointer to the resulting string dest
  */
-char *_strncat(char *det, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, dest_len = 0;
 
