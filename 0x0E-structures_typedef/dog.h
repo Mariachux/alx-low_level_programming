9dea14de456da98744f8c94404795150b117e1cb
Mariachux <ezehchukwuma@gmail.com>
2022-10-10 13:39:24 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,6 @@
  * @name: first member
  * @age: second member
  * @owner: third member
- * Description: longer description
  */
 struct dog
 {
@@ -24,7 +23,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char 8dest, char *src);
+char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
